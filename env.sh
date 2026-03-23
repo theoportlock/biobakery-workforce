7aac2a34e44223a1a78084f1a610b7a087f00342
theoportlock <theo.portlock@auckland.ac.nz>
2026-03-24 12:20:37 +1300

    Correct docker and singularity setup

--- env.sh
+++ env.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # Data path
 export INPUT="testdata/*_R{1,2}_001.fastq.gz"
 export OUTPUT="work"
 
+# Singularity
+export SINGULARITY_CACHEDIR="$PWD/images"
+
 # Configuration
 export KNEADDATA_DB="human_genome"
 export KNEADDATA_DB_DIR="db/kneaddata_db"
-export KNEADDATA_CONTAINER_CMD="singularity exec -B $PWD:/data images/kneaddata.sif"
-#export KNEADDATA_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data biobakery/kneaddata:latest"
+export KNEADDATA_URL="biobakery/kneaddata:latest"
+export KNEADDATA_CONTAINER_CMD="singularity exec -B $PWD:/data docker://$KNEADDATA_URL"
+#export KNEADDATA_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data $KNEADDATA_URL"
 
 export METAPHLAN_DB="mpa_vJan25_CHOCOPhlAnSGB_202503"
 export METAPHLAN_DB_DIR="db/metaphlan_db"
-export METAPHLAN_CONTAINER_CMD="singularity exec -B $PWD:/data images/metaphlan.sif"
-#export METAPHLAN_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data biobakery/metaphlan:latest"
+export METAPHLAN_URL="depot.galaxyproject.org/singularity/metaphlan:4.2.4--pyhdfd78af_0"
+export METAPHLAN_CONTAINER_CMD="singularity exec -B $PWD:/data docker://$METAPHLAN_URL"
+#export METAPHLAN_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data $METAPHLAN_URL"
+
+export HUMANN_DB_DIR="db/humann_db"
+export HUMANN_URL="depot.galaxyproject.org/singularity/humann:3.9--py312hdfd78af_0"
+export HUMANN_CONTAINER_CMD="singularity exec -B $PWD:/data docker://$HUMANN_URL"
+#export HUMANN_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data $HUMANN_URL"
 
 export EXECUTOR="bash"
 #export EXECUTOR="sbatch --wait"

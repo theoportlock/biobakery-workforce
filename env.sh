0fb55dcdc76b057759d24cea81820bad039c1b34
Theo Portlock <zn.tportlock@gmail.com>
2026-04-14 12:48:08 +1200

    Latest updates

--- env.sh
+++ env.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 # Data path
 export INPUT="testdata/*_R{1,2}_001.fastq.gz"
@@ -10,32 +9,33 @@ export APPTAINER_CACHEDIR="$PWD/images"
 
 # Configuration
 export KNEADDATA_URL="biobakery/kneaddata:latest"
+export KNEADDATA_DB_DIR="../databases/kneaddata_db"
 export KNEADDATA_CONTAINER_CMD="apptainer exec -B $PWD:/data -B $(realpath $KNEADDATA_DB_DIR):/kneaddata_db docker://$KNEADDATA_URL"
 #export KNEADDATA_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data $KNEADDATA_URL"
 export KNEADDATA_DB="human_genome"
-export KNEADDATA_DB_DIR="../databases/kneaddata_db"
 
 export METAPHLAN_URL="depot.galaxyproject.org/singularity/metaphlan:4.2.4--pyhdfd78af_0"
+export METAPHLAN_DB_DIR="../databases/metaphlan_db"
 export METAPHLAN_CONTAINER_CMD="apptainer exec -B $PWD:/data -B $(realpath $METAPHLAN_DB_DIR):/metaphlan_db docker://$METAPHLAN_URL"
 #export METAPHLAN_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data $METAPHLAN_URL"
 export METAPHLAN_DB="mpa_vJan25_CHOCOPhlAnSGB_202503"
-export METAPHLAN_DB_DIR="../databases/metaphlan_db"
 
 export HUMANN_URL="depot.galaxyproject.org/singularity/humann:3.9--py312hdfd78af_0"
+export HUMANN_DB_DIR="../databases/humann_db"
 export HUMANN_CONTAINER_CMD="apptainer exec -B $PWD:/data -B $(realpath $HUMANN_DB_DIR):/humann_db docker://$HUMANN_URL"
 #export HUMANN_CONTAINER_CMD="docker run --rm -v $PWD:/data -w /data $HUMANN_URL"
-export HUMANN_DB_DIR="../databases/humann_db"
 
 export BASE_URL="ubuntu"
 export BASE_CMD="apptainer exec"
 
 export EXECUTOR="bash"
+#export EXECUTOR="sbatch"
 
 # Add project paths to PATH
 export PATH="code:$PATH"
 
 # Activate Python virtual environment
-. venv/bin/activate
+source venv/bin/activate
 
 module load Parallel
 
